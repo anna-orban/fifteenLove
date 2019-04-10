849d328ce002f254ce67a96177c9d642ea883d34
Annamaria Orban <anna.orban@yahoo.com>
2019-04-10 21:30:19 +0100

    add grid

--- main.lua
+++ main.lua
@@ -1,16 +1,27 @@
 function love.load()
     love.graphics.setNewFont(30)
+
+    gridXCount = 4
+    gridYCount = 4
+
+    grid = {}
+    for y = 1, gridYCount do
+        grid[y] = {}
+        for x = 1, gridXCount do
+            grid[y][x] = x + ((y-1) * gridXCount)
+        end
+    end
 end
 
 function love.draw()
-    for y = 1, 4 do
-        for x = 1, 4 do
+    for y = 1, gridYCount do
+        for x = 1, gridXCount do
             local pieceSize = 100
             local pieceDrawSize = pieceSize - 1
             love.graphics.setColor(.4, .1, .6)
             love.graphics.rectangle('fill', (x - 1) * pieceSize, (y - 1) * pieceSize, pieceDrawSize, pieceDrawSize)
             love.graphics.setColor(1, 1, 1)
-            love.graphics.print(x + ((y - 1) * 4), (x - 1) * pieceSize, (y - 1) * pieceSize)
+            love.graphics.print(grid[y][x], (x - 1) * pieceSize, (y - 1) * pieceSize)
         end
     end
 end
